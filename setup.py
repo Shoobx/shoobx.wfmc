a8b82a92a5a38ac04013f286d2a34b1c83ef2345
Stephan Richter <stephan.richter@gmail.com>
2017-05-17 16:49:04 -0400

    Back to development: 4.0.0.sbx2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.wfmc',
-      version='4.0.0.sbx1',
+      version='4.0.0.sbx2.dev0',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       description="Workflow-Management Coalition Workflow Engine",
