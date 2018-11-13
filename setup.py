f2d13b7ce4bfcb73550bf8099c3c277d0225b3fe
Stephan Richter <stephan.richter@gmail.com>
2018-11-12 21:16:26 -0500

    Add Py 3.7 and remove Py 3.5

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='shoobx.wfmc',
-      version='4.1.2.dev0',
+      version='4.2.0.dev0',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       description="Workflow-Management Coalition Workflow Engine",
@@ -45,8 +45,8 @@ setup(name='shoobx.wfmc',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: Implementation :: CPython',
           'Natural Language :: English',
           'Operating System :: OS Independent'],
